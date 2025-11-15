e40ba25aaeccd8fdbeb389c1729000f4417a51cc
CHANSIK-CHOI <ckstlr0828@frameout.co.kr>
2025-11-16 08:36:29 +0900

    [최찬식] ui 제작 전 type 미리 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
+import { useEffect } from 'react'
+
 function App() {
+  useEffect(() => {
+    const fetchTest = async () => {
+      try {
+        const res = await fetch('https://reqres.in/api/users', {
+          headers: {
+            'x-api-key': 'reqres-free-v1',
+          },
+        })
+        const json = await res.json()
+        console.log(json, res)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    void fetchTest()
+  }, [])
   return (
     <main>
       <div className="layout-center">
