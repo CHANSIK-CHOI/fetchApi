fc079ec27f40111728b82e9cd30e5706421a9137
CHANSIK-CHOI <ckstlr0828@frameout.co.kr>
2025-11-15 22:26:20 +0900

    [최찬식] 초기 세팅 완료

--- src/hooks/useCounter.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-
-export const useCounter = (initialValue = 0) => {
-  const [count, setCount] = useState(initialValue)
-  const increment = () => setCount((previous) => previous + 1)
-  const reset = () => setCount(initialValue)
-
-  return { count, increment, reset }
-}
